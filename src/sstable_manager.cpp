c2ddce5d58ea98f433a851705968b185b9d96678
Пользователь <xannaxks@gmail.com>
2026-07-08 16:45:29 +0900

    fix(sstable_manager): methods implementation fixed

--- src/sstable_manager.cpp
+++ src/sstable_manager.cpp
@@ -18,6 +18,111 @@ std::filesystem::path SSTableManager::make_tmp_table_path(
     return dir / std::format("table-{:09}.sst.tmp", table_id);
 }
 
+Result<std::shared_ptr<SSTable>> SSTableManager::get(std::uint32_t table_id, Arena& arena)
+{
+    auto it = this->pool.find(table_id);
+    if (it != this->pool.end()) {
+        return Result<std::shared_ptr<SSTable>>::ok(it->second);
+    }
+
+    return this->open(table_id, arena);
+}
+
+Result<std::shared_ptr<SSTable>> SSTableManager::open(std::uint32_t table_id, Arena& arena)
+{
+    return this->open_impl(
+        table_id,
+        make_table_path(table_id, this->db_dir),
+        arena
+    );
+}
+
+Result<std::shared_ptr<SSTable>> SSTableManager::open( TableMeta& meta, Arena& arena)
+{
+    return this->open_impl(
+        meta.table_id,
+        meta.path,
+        arena
+    );
+}
+
+Result<std::shared_ptr<SSTable>> SSTableManager::open_impl(
+    std::uint32_t table_id,
+     std::filesystem::path& path,
+    Arena& arena
+)
+{
+	return this->open_impl(
+		table_id,
+		std::move(path),
+		arena
+	);
+}
+
+Result<std::shared_ptr<SSTable>> SSTableManager::open_impl(
+    std::uint32_t table_id,
+    std::filesystem::path&& path,
+    Arena& arena
+)
+{
+    auto it = this->pool.find(table_id);
+    if (it != this->pool.end()) {
+        return Result<std::shared_ptr<SSTable>>::ok(it->second);
+    }
+
+    auto sstable_result = SSTableLoader::load(path, arena);
+    if (!sstable_result.is_ok()) {
+        return Result<std::shared_ptr<SSTable>>::fail(
+            std::move(sstable_result.status)
+        );
+    }
+
+    auto sstable_ptr = std::make_shared<SSTable>(
+        std::move(sstable_result.value)
+    );
+
+    this->pool.emplace(table_id, sstable_ptr);
+
+    return Result<std::shared_ptr<SSTable>>::ok(sstable_ptr);
+}
+
+Result<std::optional<SSTable>> SSTableManager::build(
+    std::uint32_t table_id,
+     MemTable& mem_table,
+    Arena& arena
+)
+{
+	std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
+	std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
+
+	return SSTableBuilder::build(table_id, mem_table, path, final_path);
+}
+
+Result<std::optional<SSTable>> SSTableManager::build(
+    std::uint32_t table_id,
+     SSTableIterator& iterator,
+    Arena& arena
+)
+{
+    std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
+    std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
+
+    return SSTableBuilder::build(table_id, iterator, path, final_path);
+}
+
+Status SSTableManager::write(SSTable& sstable)
+{
+    return sstable.write();
+}
+
+SSTableManager::SSTableManager(std::filesystem::path& path)
+    : db_dir(path)
+{}
+
+SSTableManager::SSTableManager(std::filesystem::path&& path)
+    : db_dir(path)
+{}
+
 //std::vector<Status> SSTableManager::load(Arena& arena, const std::filesystem::path& root_path)
 //{
 //    std::vector<Status> load_results;
@@ -82,50 +187,50 @@ std::filesystem::path SSTableManager::make_tmp_table_path(
 //    this->immutable_pool.emplace_back(std::move(sstable));
 //}
 
-Result<std::optional<TableMeta>> SSTableManager::create_from_memtable(
-    std::uint32_t table_id,
-    const MemTable& mem_table,
-    const SSTableBuilderOptions& builder_options,
-    Arena& arena
-)
-{
-    std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
-    std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
-    SSTableBuilder builder(builder_options);
-    SSTable sstable = std::move(builder.build_from_memtable(mem_table));
-
-    Result<TableMeta> result_table_meta = make_table_meta(sstable, 0, arena);
-    if (!result_table_meta.is_ok())
-        return Result<std::optional<TableMeta>>::fail(std::move(result_table_meta.status));
-
-    SSTableWriter writer(path, final_path);
-    Status write_result = writer.write_and_flush(sstable);
-    if (!write_result.is_ok())
-        return Result<std::optional<TableMeta>>::fail(std::move(write_result));
-
-    return Result<std::optional<TableMeta>>::ok(std::move(result_table_meta.value));
-}
-
-Result<std::optional<TableMeta>> SSTableManager::create_from_iterator(
-    std::uint32_t table_id,
-    const SSTableI& mem_table,
-    const SSTableBuilderOptions& builder_options,
-    Arena& arena
-)
-{
-    std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
-    std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
-    SSTableBuilder builder(builder_options);
-    SSTable sstable = std::move(builder.build_from_memtable(mem_table));
-
-    Result<TableMeta> result_table_meta = make_table_meta(sstable, 0, arena);
-    if (!result_table_meta.is_ok())
-        return Result<std::optional<TableMeta>>::fail(std::move(result_table_meta.status));
-
-    SSTableWriter writer(path, final_path);
-    Status write_result = writer.write_and_flush(sstable);
-    if (!write_result.is_ok())
-        return Result<std::optional<TableMeta>>::fail(std::move(write_result));
-
-    return Result<std::optional<TableMeta>>::ok(std::move(result_table_meta.value));
-}
+//Result<std::optional<TableMeta>> SSTableManager::create_from_memtable(
+//    std::uint32_t table_id,
+//    const MemTable& mem_table,
+//    const SSTableBuilderOptions& builder_options,
+//    Arena& arena
+//)
+//{
+//    std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
+//    std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
+//    SSTableBuilder builder(builder_options);
+//    SSTable sstable = std::move(builder.build_from_memtable(mem_table));
+//
+//    Result<TableMeta> result_table_meta = make_table_meta(sstable, 0, arena);
+//    if (!result_table_meta.is_ok())
+//        return Result<std::optional<TableMeta>>::fail(std::move(result_table_meta.status));
+//
+//    SSTableWriter writer(path, final_path);
+//    Status write_result = writer.write_and_flush(sstable);
+//    if (!write_result.is_ok())
+//        return Result<std::optional<TableMeta>>::fail(std::move(write_result));
+//
+//    return Result<std::optional<TableMeta>>::ok(std::move(result_table_meta.value));
+//}
+//
+//Result<std::optional<TableMeta>> SSTableManager::create_from_iterator(
+//    std::uint32_t table_id,
+//    const SSTableI& mem_table,
+//    const SSTableBuilderOptions& builder_options,
+//    Arena& arena
+//)
+//{
+//    std::filesystem::path path = SSTableManager::make_tmp_table_path(table_id, this->db_dir);
+//    std::filesystem::path final_path = SSTableManager::make_table_path(table_id, this->db_dir);
+//    SSTableBuilder builder(builder_options);
+//    SSTable sstable = std::move(builder.build_from_memtable(mem_table));
+//
+//    Result<TableMeta> result_table_meta = make_table_meta(sstable, 0, arena);
+//    if (!result_table_meta.is_ok())
+//        return Result<std::optional<TableMeta>>::fail(std::move(result_table_meta.status));
+//
+//    SSTableWriter writer(path, final_path);
+//    Status write_result = writer.write_and_flush(sstable);
+//    if (!write_result.is_ok())
+//        return Result<std::optional<TableMeta>>::fail(std::move(write_result));
+//
+//    return Result<std::optional<TableMeta>>::ok(std::move(result_table_meta.value));
+//}
