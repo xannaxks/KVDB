bb43896dd4876471a754c9e645f5d26ea9ebd030
Пользователь <xannaxks@gmail.com>
2026-07-17 16:38:43 +0900

    fix(file_helpers): error handling

--- src/file_helpers.cpp
+++ src/file_helpers.cpp
@@ -1,17 +1,21 @@
+#define NOMINMAX
 #include "file_helpers.h"
 
 #include <cassert>
 #include <cstdint>
+#include <format>
 #include <cstddef>
 #include <fstream>
+#include <algorithm>
 #include <vector>
-
+#include <limits>
 
 Status fits_in_block(std::uint64_t offset,
     std::size_t size,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
 
     if (size == 0)
         return Status::ok();
@@ -32,39 +36,56 @@ Status align_to_block_boundary(WritableFile& file,
     std::uint64_t& offset,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
     
 	Result<std::uint64_t> get_position_result = file.current_position();
     if(!get_position_result.is_ok())
         return get_position_result.status;
 
-    assert(offset == get_position_result.value);
+    if (offset != get_position_result.value)
+        return Status{ StatusCode::InvalidOffset, "manually tracked offset is not equal to file cursor" };
 
     const std::uint64_t in_block = offset % block_size;
 
     if (in_block == 0)
         return Status::ok();
 
-    const std::uint64_t pad = block_size - in_block;
+    std::uint64_t pad = block_size - in_block;
 
-    std::vector<char> poison(static_cast<std::size_t>(pad),
+    std::vector<char> poison(static_cast<std::size_t>(std::min(4096ull, pad)),
         static_cast<char>(0xCD));
 
-    return file.append(poison.data(), poison.size(), offset);
-}
+    Status status;
 
+    while (pad)
+    {
+        std::size_t sz = std::min(pad, poison.size());
+
+        status = file.append(poison.data(), sz, offset);
+        if (!status.is_ok())
+            return status;
+
+        pad -= sz;
+    }
+    
+    return Status::ok();
+}
 
 Status ensure_fits_in_block(WritableFile& file,
     std::size_t size,
     std::uint64_t& offset,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
+
     Result<std::uint64_t> get_position_result = file.current_position();
     if (!get_position_result.is_ok())
         return get_position_result.status;
 
-    assert(offset == get_position_result.value);
+    if (offset != get_position_result.value)
+        return Status{ StatusCode::InvalidOffset, "manually tracked offset is not equal to file cursor" };
 
     if(size > block_size)
         return Status{StatusCode::SizeExceedsBlockSize, "Size exceeds block size"};
@@ -89,12 +110,24 @@ Status align_to_block_boundary(ReadableFile& file,
     std::uint64_t& offset,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
 
     const std::uint64_t in_block = offset % block_size;
 
     if (in_block == 0)
         return Status::ok();
+
+    if (offset > std::numeric_limits<std::uint64_t>::max() - (block_size - in_block))
+        return Status{
+            StatusCode::DataTypeOverflow,
+            std::format(
+                "offset + (block_size - in_block) overflow uint64_t: {} + ({} - {})",
+                offset,
+                block_size,
+                in_block
+            )
+    };
 
     const std::uint64_t new_offset = offset + (block_size - in_block);
     offset = new_offset;
@@ -108,7 +141,8 @@ Status ensure_fits_in_block(ReadableFile& file,
     std::uint64_t& offset,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
 
     if (size > block_size)
         return Status{StatusCode::SizeExceedsBlockSize, "Size exceeds block size"};
@@ -128,12 +162,23 @@ Status move_g_to_next_block(ReadableFile& file,
     std::uint64_t& offset,
     std::uint32_t block_size)
 {
-    assert(block_size > 0);
+    if (block_size == 0)
+        return Status{ StatusCode::InvalidArgument, "block size can't be equal to zero" };
 
     const std::uint64_t in_block = offset % block_size;
 
     const std::uint64_t jump =
         (in_block == 0) ? block_size : (block_size - in_block);
+
+    if (offset > std::numeric_limits<std::uint64_t>::max() - jump)
+        return Status{
+            StatusCode::DataTypeOverflow,
+            std::format(
+                "offset + jump overflows std::uint64_t: {} + {}",
+                offset,
+                jump
+            )
+        };
 
     const std::uint64_t new_offset = offset + jump;
 
