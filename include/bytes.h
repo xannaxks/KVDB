a289b3a7cf9ce76defb9fd2bdf2f571629808514
xannaxks <xannaxks@gmail.com>
2026-07-24 15:50:00 +0900

    untracked: api.h api.cpp bytes.h bytes.cpp main.h main.cpp

--- include/bytes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-using Bytes = std::vector<std::byte>;
-void write_to_bytes(Bytes& bytes, const std::string& value);
-void write_to_string(std::string& buffer, const Bytes& bytes);
