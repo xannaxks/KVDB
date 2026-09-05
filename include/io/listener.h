e4811851f2f68383acb82cf5153fb6de25a66e45
xannaxks <xannaxks@gmail.com>
2026-09-05 19:31:56 +0900

    feat: add listener

--- include/io/listener.h
+++ include/io/listener.h
@@ -15,21 +15,29 @@
 #endif
 
 #include <cerrno>
+#include <optional>
 #include <vector>
+
+#ifdef _WIN32
+using Socket = SOCKET;
+using InvalidSocket = INVALID_SOCKET;
+using SocketError = SOCKET_ERROR;
+#else
+using Socket = int;
+using InvalidSocket = -1;
+using SocketError = -1;
+#endif
+
 
 class Listener
 {
 private:
-
-#ifdef _WIN32
-	SOCKET fd_ = INVALID_SOCKET;
-#else
-	int fd_ = -1;
-#endif
-
+	Socket fd_ = InvalidSocket;
 	std::uint16_t port;
 
 	void close_socket();
+	void close_socket_noexcept() noexcept;
+
 public:
 	Listener(std::uint16_t port)
 		: port(port)
@@ -43,12 +51,8 @@ public:
 	* @brief Accept one pending connection.
 	* @return The newly connected socket.
 	*/
-	int accept();
+	std::optional<Socket> accept_connection();
 
 	// @brief descriptor getters
-#ifdef _WIN32
-	const SOCKET& socket() const noexcept;
-#else
-	int fd() const noexcept;
-#endif
+	Socket get_fd() const noexcept;
 };
--- src/io/listener.cpp
+++ src/io/listener.cpp
@@ -7,83 +7,231 @@
 #include <cstdint>
 #include <iostream>
 #include <system_error>
+#include <optional>
 
 Listener::Listener(std::uint16_t port)
 	: port(port)
 {
-	try
+#ifdef _WIN32
+	fd_ = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+#else
+	fd_ = ::socket(
+		AF_INET,
+		SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC,
+		0
+	);
+#endif
+
+	if (fd_ == InvalidSocket)
 	{
-		fd_ = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
-	}
-	catch (const std::exception& e)
-	{
-		/**
-		* @todo Consider making log file with errors or use initialization function to return appropriate status
-		*/
-		std::cerr << "Error: " << e.what() << std::endl;
-		return;
-	}
-	
-	if (fd_ == -1)
-	{
-		int current_error = errno;
-		std::cerr << "Error: " << std::system_error(current_error, "Error, failed to create socket: ");
-		return;
+		int current_error = get_last_socket_error();
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to create socket"
+		);
 	}
 
-	if(::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)
-	{
 #ifdef _WIN32
-		closesocket(fd_);
-#else
-		::close(fd_);
+	u_long mode = 1;
+	
+	if (::ioctlsocket(fd_, FIONBIO, &mode) == SocketError)
+	{
+		int current_error = get_last_socket_error();
+
+		this->close_socket_noexcept();
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to switch socket to non blocking mode"
+		);
+	}
 #endif
-		return syscall_error(StatusCode::SocketOptionFailed, "failed to set socket options");
+
+	const char yes = 1;
+
+	if(::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == SocketError)
+	{
+		const int current_error = get_last_socket_error();
+
+		this->close_socket_noexcept();
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to set socket options"
+		);
 	}
 
 	sockaddr_in address{};
 
 	address.sin_family = AF_INET;
-	address.sin_addrs.s_addr = htonl(INADDR_ANY);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(port);
 
-	if (::bind(fd_, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == -1)
+	if (::bind(fd_, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == SocketError)
 	{
-		this->close_socket();
-		return syscall_error(StatusCode::SocketBindFailed, "failed to bind socket to port")
+		const int current_error = get_last_socket_error();
+
+		this->close_socket_noexcept();
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to bind socket to address"
+		);
 	}
 
-
-	if (::listen(fd_, SOMAXCONN) == -1)
+	if (::listen(fd_, SOMAXCONN) == SocketError)
 	{
-		this->close_socket();
+		const int current_error = get_last_socket_error();
+
+		this->close_socket_noexcept();
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to set socket to listen mode"
+		);
 	}
 }
 
 void Listener::close_socket()
 {
+	if (fd_ == InvalidSocket)
+		return;
+
 #ifdef _WIN32
-	if (::closesocket(fd_) == SOCKET_ERROR)
+	const int result = ::closesocket(fd_);
+#else
+	const int result = ::close(fd_);
+#endif
+
+	if (result == -1)
 	{
-		const DWORD err = ::GetLastError();
-		// @todo Consider outputing to log file.
-		std::cerr << std::system_category().message(err) << std::endl;
-		throw std::runtime_error(std::system_category().message(err));
-	}
-#else 
-	if (::close(fd_) == -1)
-	{
-		int error = errno;
-		
+		const int error = get_last_socket_error();
+
+		fd_ = InvalidSocket;
+
+#ifndef _WIN32
 		if (error == EINTR)
 			return;
-		
-		if (error == EBADF)
+#endif
+
+		throw std::system_error(
+			error,
+			std::system_category(),
+			"failed to close socket"
+		);
+	}
+
+	fd_ = InvalidSocket;
+}
+
+void Listener::close_socket_noexcept() noexcept
+{
+	if (fd_ == InvalidSocket)
+		return;
+
+	// consider logging into logging system in case of failure, but don't throw
+#ifdef _WIN32
+	::closesocket(fd_);
+#else
+	::close(fd_);
+#endif
+
+	fd_ = InvalidSocket;
+}
+
+Listener::~Listener()
+{
+	this->close_socket_noexcept();
+}
+
+Socket Listener::get_fd() const noexcept
+{
+	return this->fd_;
+}
+
+int get_last_os_error()
+{
+#ifdef _WIN32
+	return static_cast<int>(::GetLastError());
+#else
+	return errno;
+#endif
+}
+
+int get_last_socket_error()
+{
+#ifdef _WIN32
+	return static_cast<int>(::WSAGetLastError());
+#else
+	return errno;
+#endif
+}
+
+std::optional<Socket> Listener::accept_connection()
+{
+#ifdef _WIN32
+	Socket client = ::accept(this->fd_, nullptr, nullptr);
+	
+	if (client == InvalidSocket)
+	{
+		int current_error = get_last_socket_error();
+
+		if (current_error == WSAEWOULDBLOCK)
+			return std::nullopt;
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to estabilish connection with client"
+		);
+	}
+
+	u_long mode = 1;
+
+	if (::ioctlsocket(client, FIONBIO, &mode) == SocketError)
+	{
+		int current_error = get_last_socket_error();
+
+		::closesocket(client); // we dont care about potential errors produced by closesocket, cuz we throw top layer error
+
+		throw std::system_error(
+			current_error,
+			std::system_category(),
+			"failed to switch connected socket mode"
+		);
+	}
+
+	return client;
+#else
+	for (;;) { // outter infinite loop, needd to handle EINTR
+		Socket client = ::accept4(fd_, nullptr, nullptr, SOCK_NONBLOCK | SOCK_CLOEXEC);
+
+		if (client == InvalidSocket)
 		{
-			std::cerr << std::system_category().message(error) << std::endl;
-			throw std::runtime_error(std::system_category().message(error));
+			int current_error = get_last_socket_error();
+
+			// nothing, probably was interrupted by sys call, not really exception in ordinary way
+			if (current_error == EINTR)
+				continue;
+
+			if (current_error == EAGAIN || current_error == EWOULDBLOCK) // no client waiting to estabilish connection
+				return std::nullopt; // returning std::nullopt in case if no client waiting to establish connection, in case of failure - throw
+
+			throw std::system_error(
+				current_error,
+				std::system_category(),
+				"failed to estabilish connection with client"
+			);
 		}
-		
+
+		return client;
 	}
 #endif
 }
+
