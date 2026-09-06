4205542caa6b72ea9b4ee4acc252f4d51bc7a7c6
xannaxks <xannaxks@gmail.com>
2026-09-07 02:49:47 +0900

    feat: define event loop

--- include/io/event_loop.h
+++ include/io/event_loop.h
@@ -3,14 +3,40 @@
 #include "connection_manager.h"
 #include "listener.h"
 
+enum class EventFlag : std::uint32_t
+{
+	None = 0,
+	Readable = 1 << 0, // EPOLLIN
+	Writable = 1 << 1, // EPOLLOUT
+	Error = 1 << 2, // EPOLLERR
+	Hangup = 1 << 3, // EPOLLHUP
+};
+
+struct Event
+{
+	Socket fd;
+	EventFlag flags;
+};
+
 class EventLoop
 {
 public:
-	void run();
+	void run(); // delegates to poller_ and handles events
+
+	void handle_event(const Event& event);
+
+	void handle_eventfd();
+	void handle_on_readable(Socket fd);
+	void handle_on_writable(Socket fd);
+	void handle_accept();
+	void handle_error(Socket fd);
 
 private:
-	int epoll_fd_;
 
+	// epolls, don't own and dont know anythning about fds, just wait for events and delegate handling to event_loop
+	Poller poller_;
+
+	Bridge eventfd_;
 	Listener listener_;
 	ConnectionManager connection_manager_;
 };
